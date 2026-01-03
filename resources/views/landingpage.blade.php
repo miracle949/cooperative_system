e1c9d8328e5be567f8f75f034f27dff93592071e
Rogelio Amoyan <rogelioamoyan123@gmail.com>
2026-01-03 20:46:13 +0800

    Update landing page and member portal

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kingsland Pala Pala Cooperative</title>
+    <title>Kingsland Pala Pala Cooperative Management System</title>
     
     <link rel="icon" href="images/websitelogo.png" type="image/png">
 
